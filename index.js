92996dddd18cd08f05c24d366bcdc1aef0ed3445
amandollar <toxicempire3@gmail.com>
2025-06-20 00:25:40 +0530

    Adeed cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js'
 import userRouter from './route/userRoutes.js';
 import blogRouter from './route/blogsRoutes.js';
+import cors from 'cors';
 dotenv.config();
 
 
@@ -10,8 +11,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
 app.use('/api/users', userRouter);  
 app.use('/api/blogs', blogRouter); 
 
